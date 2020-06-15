88c17db87290f4e91b9ab576315ba69f81f24fa5
Hien Nguyen <hien_d_nguyen@outlook.com>
2020-06-15 15:09:51 -0600

    add search capability to SearchBox

--- src/App.js
+++ src/App.js
@@ -3,14 +3,36 @@ import CardList from "./components/CardList";
 import SearchBox from "./components/SearchBox";
 import { robots } from "./store/robots";
 
-const App = () => {
-  return (
-    <div className="tc">
-      <h1>My Team</h1>
-      <SearchBox />
-      <CardList items={robots} />
-    </div>
-  );
-};
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      robots,
+      searchTerm: "",
+    };
+  }
+
+  onSearchChange = (event) => {
+    this.setState({ searchTerm: event.target.value });
+  };
+
+  render() {
+    const filtered = this.state.robots.filter(
+      (robot) =>
+        robot.name
+          .toLowerCase()
+          .includes(this.state.searchTerm.toLowerCase()) ||
+        robot.email.toLowerCase().includes(this.state.searchTerm.toLowerCase())
+    );
+
+    return (
+      <div className="tc">
+        <h1>My Team</h1>
+        <SearchBox searchChange={this.onSearchChange} />
+        <CardList items={filtered} />
+      </div>
+    );
+  }
+}
 
 export default App;
