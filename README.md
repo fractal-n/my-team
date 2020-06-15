9564fded0e8df9e75612a74db479d8c3092e59ba
Hien Nguyen <hien_d_nguyen@outlook.com>
2020-06-15 16:18:45 -0600

    call from mock api and add scrollable list

--- README.md
+++ README.md
@@ -9,3 +9,10 @@ This is a simple frontend project to learn React and Redux.
 ReactJS
 Redux
 Tachycon (CSS toolkit)
+
+## Mockup tools
+
+- Image: [RoboHash](https://robohash.org)
+- Data: [JSONPlaceholder](https://jsonplaceholder.typicode.com/)
+
+## Deployment
--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import React from "react";
 import "./App.css";
+import Scroll from "./components/Scroll.js";
 import CardList from "./components/CardList";
 import SearchBox from "./components/SearchBox";
-import { robots } from "./store/robots";
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      robots,
+      robots: [],
       searchTerm: "",
     };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => this.setState({ robots: users }));
   }
 
   onSearchChange = (event) => {
@@ -26,11 +32,17 @@ class App extends React.Component {
         robot.email.toLowerCase().includes(this.state.searchTerm.toLowerCase())
     );
 
+    if (this.state.robots.length === 0) {
+      return <h1 className="f1 tc">Loading</h1>;
+    }
+
     return (
       <div className="tc">
         <h1 className="f1">My Team</h1>
         <SearchBox searchChange={this.onSearchChange} />
-        <CardList items={filtered} />
+        <Scroll>
+          <CardList items={filtered} />
+        </Scroll>
       </div>
     );
   }
