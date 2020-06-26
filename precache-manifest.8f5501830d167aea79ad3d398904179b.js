ac5a597830945927e0729fb2c25077439672dce3
Hien Nguyen <hien_d_nguyen@outlook.com>
2020-06-25 21:48:24 -0600

    'Deploy'

---
+++ precache-manifest.8f5501830d167aea79ad3d398904179b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5828deb8a3f36f8f2c288d2a17fa9590",
+    "url": "/my-team/index.html"
+  },
+  {
+    "revision": "0e69373c0ddf928d41b3",
+    "url": "/my-team/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "45ada71f26d628013804",
+    "url": "/my-team/static/css/main.556afe91.chunk.css"
+  },
+  {
+    "revision": "0e69373c0ddf928d41b3",
+    "url": "/my-team/static/js/2.c8c579dc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/my-team/static/js/2.c8c579dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45ada71f26d628013804",
+    "url": "/my-team/static/js/main.d4db98d0.chunk.js"
+  },
+  {
+    "revision": "ddaabe85dde65d77a83c",
+    "url": "/my-team/static/js/runtime-main.463f9289.js"
+  },
+  {
+    "revision": "82e09f7862a1e26fb7dfe1d0f8adc87f",
+    "url": "/my-team/static/media/SEGA.82e09f78.TTF"
+  }
+]);
